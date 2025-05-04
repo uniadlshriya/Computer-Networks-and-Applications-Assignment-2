ad8850c0341801fe590b2b47e6147fa0a947654e
Shriya Somasundara Narasimha <a1741690@adelaide.edu.au>
2025-05-04 22:12:08 +0930

    Implemented input function, which is responsible for handling ACK packets that A the sender  receives from B the receiver in the SR protocol

--- sr.c
+++ sr.c
@@ -13,7 +13,7 @@
 #define WINDOWSIZE 6    /* the maximum number of buffered unacked packet */
 #define SEQSPACE 7      /* the min sequence space for GBN must be at least windowsize + 1 */
 #define NOTINUSE (-1)   /* used to fill header fields that are not being used */
-#define BUFSIZE 48      /* keeping a sizeable buffer which is much bigger than the window size to store application messages */
+//#define BUFSIZE 48      /* keeping a sizeable buffer which is much bigger than the window size to store application messages */
 
 
 
@@ -45,8 +45,9 @@ bool IsCorrupted(struct pkt packet)
 
 
 /********* Sender (A) variables and functions ************/
-static struct pkt ABuffer[BUFSIZE]; /* array for storing packets waiting for ACK */
-static bool srAcked[BUFSIZE];    /* adding an array to track each packet which are acknowledged (differs from GBN when they are cumulatively acked) */
+static struct pkt ABuffer[SEQSPACE]; /* array for storing packets waiting for ACK */
+static float srSent[SEQSPACE];  /* Array for tracking sent message */
+static bool srAcked[SEQSPACE];    /* adding an array to track each packet which are acknowledged (differs from GBN when they are cumulatively acked) */
 
 static int base = 0; /* beginning of the sliding window */ 
 static int A_nextseqnum = 0; /* the next sequence number to be used by the sender */
@@ -77,21 +78,22 @@ void A_output(struct msg message)
 
     /* put packet in window buffer */
     
-    int windowlast = A_nextseqnum % BUFSIZE; 
+    int windowlast = A_nextseqnum % SEQSPACE; 
     ABuffer[windowlast] = sendpkt;
     srAcked[windowlast] = false;
+    srSent[windowlast] = true;
+    
 
     /* send out packet */
     if (TRACE > 0)
       printf("Sending packet %d to layer 3\n", sendpkt.seqnum);
     tolayer3 (A, sendpkt);
 
-    /* start timer if first packet in window */
-    if (base == A_nextseqnum)
-      starttimer(A,RTT);
+    /* start timer for this packet */
+    starttimer(A,RTT);
 
     /* get next sequence number */
-    A_nextseqnum = (A_nextseqnum + 1) % BUFSIZE;  
+    A_nextseqnum = (A_nextseqnum + 1) % SEQSPACE;  
   }
   /* if blocked,  window is full */
   else {
@@ -107,14 +109,64 @@ void A_output(struct msg message)
 */
 void A_input(struct pkt packet)
 {
+  int ackcount = 0;
+  int i;
+
+  /* if received ACK is not corrupted */ 
+  if (!IsCorrupted(packet)) {
+    if (TRACE > 0)
+      printf("----A: uncorrupted ACK %d is received\n",packet.acknum);
+    total_ACKs_received++;
+
+    /* check packet for ACK*/
+    if (!srAcked[packet.acknum]) {
+       srAcked[packet.acknum] = true;
+        /* packet is a new ACK */
+        if (TRACE > 0)
+           printf("----A: ACK %d is not a duplicate\n",packet.acknum);
+        new_ACKs++;
+        stoptimer(A); /* Stop timer if they are acked, Timer is not restarted here */
+
+        /* if the packed is acked then updated acked and sent */
+        while(srAcked[base]) {
+          srAcked[base] = false;
+          srSent[base] = false;
+          base = (base+1)%SEQSPACE;
+          }
+        }
+        else
+          if (TRACE > 0)
+        printf ("----A: duplicate ACK received, do nothing!\n");
+  }
+  else 
+    if (TRACE > 0)
+      printf ("----A: corrupted ACK is received, do nothing!\n");
+
   
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt(void)
 {
-  
+  int i;
+
+  if (TRACE > 0)
+    printf("----A: time out,resend packets!\n");
+
+  for(i=0; i<SEQSPACE; i++) {
+     if (srSent[i] && !srAcked[i]) {
+        if (TRACE > 0)
+            printf ("---A: resending packet %d\n", (ABuffer[i]));
+
+        tolayer3(A,ABuffer[i]);
+        packets_resent++;
+    }
+   }
+  starttimer(A,RTT);
+
 }       
+
+
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
@@ -123,8 +175,9 @@ void A_init(void)
   /* initialise A's window, base, Timers and packets  */
   A_nextseqnum = 0;  /* A starts with seq num 0 */
   base = 0;
-  for (int i = 0; i < BUFSIZE; i++) {
-       srAcked[i] = true; /* Intializing all packets to acked */
+  for (int i = 0; i < SEQSPACE; i++) {
+      //  srTimers[i] = 0.0; /* Initialize the timers to 0 */
+       srAcked[i] = false; /* Intializing all packets to false */
   } 
 }
 
@@ -132,8 +185,8 @@ void A_init(void)
 
 /********* Receiver (B)  variables and procedures ************/
 
-// static int expectedseqnum; /* the sequence number expected next by the receiver */
-// static int B_nextseqnum;   /* the sequence number for the next packets sent by B */
+static int expectedseqnum; /* the sequence number expected next by the receiver */
+static int B_nextseqnum;   /* the sequence number for the next packets sent by B */
 
 
 /* called from layer 3, when a packet arrives for layer 4 at B*/
